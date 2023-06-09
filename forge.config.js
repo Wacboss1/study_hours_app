8ffff56c9a4fa48568fc2d5d06037e28495fbc5c
William Clemmons Jr <william.clemmons.jr@gmail.com>
2023-06-09 14:02:14 -0500

    Created forge.config.js to better control how things are packaged

---
+++ forge.config.js
@@ -0,0 +1,35 @@
+
+module.exports = {
+    packagerConfig: {
+        extraResource: [
+            "src/Backend/dist/backend"
+        ]
+    },
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                name: "electron_quick_start"
+            }
+        },
+        {
+            name: "@electron-forge/maker-zip",
+            platforms: [
+                "darwin"
+            ]
+        },
+        {
+            name: "@electron-forge/maker-deb",
+            config: {}
+        },
+        {
+            name: "@electron-forge/maker-rpm",
+            config: {}
+        }
+    ],
+    hooks:{
+        prePackage: async (platform, arch) => {
+
+        }
+    }
+}
